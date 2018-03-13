823fa2a1e563cf10cff3bb16bce6d3bd8cdfccdd
Ilya <ilya@sidorchik.ru>
2018-03-14 00:27:34 +0300

    Bigger logo and part of left col

---
+++ 2/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta http-equiv="content-type" content="text/html;charset=utf-8">
+        <meta content="width=device-width, initial-scale=1" name="viewport">
+        <title>Центральный банк Российской Федерации</title>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link href="less/styles.less" rel="stylesheet/less" type="text/css">
+        <link href="http://cbr.ru/f/r/global/icomoon-styles.min.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="js/script.js">
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/less.js/3.0.0/less.min.js"></script>
+    </head>
+    <body>
+    <div id="page">
+        <div id="layout">
+            <header>
+                <div class="main">
+                    <div class="logo no_underline">
+                        <b>
+                            <span class="ico-logo"></span>
+                        </b>
+                    </div>
+                </div>
+                <div class="extra">
+                    <div class="inner">
+                        <div class="important-links">
+                            <span class="item ip">
+                                <a href="#">Интернет-приёмная</a>
+                            </span>
+                            <span class="item">
+                                <a href="#">Ответы на вопросы</a>
+                            </span>
+                        </div>
+                        <div class="social no_underline"></div>
+                        <ul class="langs no_visited"></ul>
+                        <div class="search-container"></div>
+                    </div>
+                </div>
+            </header>
+        </div>
+    </div>
+    </body>
+</html>
