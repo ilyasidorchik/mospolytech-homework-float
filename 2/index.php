ba6bf40faa5712bab74ec5e9424abafda3a2b6e5
Ilya <ilya@sidorchik.ru>
2018-03-21 00:47:30 +0300

    Test after repo renaming

---
+++ 2/index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta http-equiv="content-type" content="text/html;charset=utf-8">
+        <meta content="width=device-width, initial-scale=1" name="viewport">
+        <title>Центральный банк Российской Федерации</title>
+        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+        <link href="less/styles.less" rel="stylesheet/less" type="text/css">
+        <link href="http://cbr.ru/f/r/global/icomoon-styles.min.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="js/script.js">
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/less.js/3.0.0/less.min.js"></script>
+    </head>
+    <body>
+    <div id="page">
+        <div id="layout">
+            <header>
+                <div class="main">
+                    <div class="logo no_underline">
+                        <b>
+                            <span class="ico-logo"></span>
+                        </b>
+                    </div>
+                </div>
+                <div class="extra">
+                    <div class="inner">
+                        <div class="important-links">
+                            <span class="item ip">
+                                <a href="#">Интернет-приёмная</a>
+                            </span>
+                            <span class="item">
+                                <a href="#">Ответы на вопросы</a>
+                            </span>
+                        </div>
+                        <div class="social no_underline">
+                            <div class="networks">
+                                <a href="https://www.facebook.com/cbr.ru/" target="_blank" class="icon_fb">
+                                    <i class="ico-social ico-social-fb" title="Facebook"></i>
+                                </a>
+                                <a href="https://twitter.com/bank_of_russia" target="_blank" class="icon_tw">
+                                    <i class="ico-social ico-social-tw" title="Twitter"></i>
+                                </a>
+                                <a href="https://www.youtube.com/user/BankofRussia" target="_blank" class="icon_youtube">
+                                    <i class="ico-social ico-social-yt" title="YouTube"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <ul class="langs no_visited">
+                            <li class="selected"><b><!--noindex-->RU<!--/noindex--></b></li>
+                            <li><a href="#"><!--noindex-->EN<!--/noindex--></a></li>
+                        </ul>
+                        <div class="search-container">
+                            <form action="/search/" method="get" class="search">
+                                <div class="search-field">
+                                    <input type="text" class="text empty" results="10" placeholder="Поиск" name="text">
+                                    <button class="search-btn" type="submit"><i class="ico-search"></i></button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <nav class="no_visited">
+                <ul class="quick_menu">
+                    <li><a href="/today/"><!--noindex-->Банк России сегодня<!--/noindex--></a></li>
+                    <li><a href="/DKP/"><!--noindex-->Денежно-кредитная политика<!--/noindex--></a></li>
+                    <li><a href="/Bank-notes_coins/"><!--noindex-->Банкноты и&nbsp;монеты<!--/noindex--></a></li>
+                    <li><a href="/analytics/"><!--noindex-->Информационно-аналитические материалы<!--/noindex--></a></li>
+                    <li><a href="/credit/"><!--noindex-->Информация по&nbsp;кредитным организациям<!--/noindex--></a></li>
+                    <li><a href="/finmarket/"><!--noindex-->Финансовые рынки<!--/noindex--></a></li>
+                    <li><a href="/fintech/"><!--noindex-->Финтех<!--/noindex--></a></li>
+                    <li><a href="/PSystem/"><!--noindex-->Национальная платежная система<!--/noindex--></a></li>
+                    <li><a href="/ckki/"><!--noindex-->Кредитные истории<!--/noindex--></a></li>
+                    <li><a href="/statistics/"><!--noindex-->Статистика<!--/noindex--></a></li>
+                    <li><a href="/ec_research/"><!--noindex-->Экономические исследования<!--/noindex--></a></li>
+                    <li><a href="/publ/"><!--noindex-->Издания Банка России<!--/noindex--></a></li>
+                    <li><a href="/tubr/"><!--noindex-->Территориальные учреждения<!--/noindex--></a></li>
+                    <li><a href="/bankmuseum/"><!--noindex-->Музейно-экспозиционный фонд<!--/noindex--></a></li>
+                    <li><a href="/press/"><!--noindex-->Пресс-служба<!--/noindex--></a></li>
+                    <li><a href="/anticor/"><!--noindex-->Противодействие коррупции<!--/noindex--></a></li>
+                </ul>
+                <div class="hr"></div>
+                <div class="hotline">
+                    <ul class="quick_menu">
+                        <li><a href="/analytics/?PrtId=na_vr">Официальное опубликование нормативных актов Банка России</a></li>
+                        <li><a href="/today/?PrtId=irp">Отношения с инвесторами</a></li>
+                        <li><a href="/lk_uio/">Личный кабинет</a></li>
+                    </ul>
+                    <div class="hr"></div>
+                    <ul class="quick_menu">
+                        <li><a href="/links/fmp_check/">Проверить участника финансового рынка</a></li>
+                    </ul>
+                    <div class="hr"></div>
+                    <div>
+                        <p><a class="no_visited" href="/contacts/">Контактная информация</a></p>
+                        <address>
+                            <span class="phone">+7 495 771-91-00</span><br>
+                            <span class="phone">8 800 250-40-72</span>
+                        </address>
+                    </div>
+                </div>
+            </nav>
+            <main>
+                <div id="content">
+                    <div class="col widgets no_visited">
+                        <div class="group static opened">
+                            <div class="wrap">
+                                <div class="wrap2">
+                                    <div class="widget opened">
+                                        <div class="content">
+                                            <table>
+                                                <tbody>
+                                                    <tr>
+                                                        <td class="title"><a href="/press/keypr/">
+                                                                Ключевая ставка  с  12.02.2018</a>,
+                                                            <ins>%</ins>
+                                                        </td>
+                                                        <td>7,50</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                    <div class="hr"></div>
+                                    <div class="widget opened type_table name_inflation">
+                                        <div class="content">
+                                            <table>
+                                                <tbody>
+                                                <tr>
+                                                    <td class="title">
+                                                        <div class="has_bubble in_text"><span class="pseudo">Инфляция</span>&nbsp;<ins>февраль 2018 года, %</ins>
+                                                            <div class="bubble">
+                                                                <p>
+                                                                    Показатель инфляции отражает темп прироста потребительских цен к соответствующему месяцу предыдущего года
+                                                                </p>
+                                                                <p class="link">
+                                                                    (источник:  <a href="http://www.gks.ru/wps/wcm/connect/rosstat_main/rosstat/ru/statistics/tariffs/">Федеральная служба государственной статистики</a>)
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                    <td>2,2</td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="title">
+                                                        <div class="has_bubble in_text"><span class="pseudo">Цель по инфляции,</span>&nbsp;<ins>%</ins>
+                                                            <div class="bubble">
+                                                                <p>
+                                                                    Цель по инфляции устанавливается для показателя инфляции, отражающего темп прироста потребительских цен к соответствующему месяцу предыдущего года
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                    <td>4,0</td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="widgets-draggable">
+                            
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+    </body>
+</html>
